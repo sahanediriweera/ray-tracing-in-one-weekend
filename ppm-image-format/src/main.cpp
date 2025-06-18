3bc71508408e08fbf3dca50b476b2312d532cb0a
Sahan <rsahanediriweera@gmail.com>
2025-06-18 22:38:02 +0530

    feat:modify-main

--- ppm-image-format/src/main.cpp
+++ ppm-image-format/src/main.cpp
@@ -1,30 +1,23 @@
+#include "lib/color.h"
+#include "lib/vec3.h"
+
 #include <iostream>
 
 int main() {
 
-    // Image
-
     int image_width = 256;
     int image_height = 256;
 
-    // Render
+    std::cout << "P3\n" << image_width  << ' ' << image_height << "\n255\n";
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for(int j =0;j<image_height;j++){
+        std::clog << "\rScanlines remaining: " << (image_height -j) << ' ' << std::flush;
 
-    for (int j = 0; j < image_height; j++) {
-        std::clog <<"\rScanlines remaining : " << (image_height -j) << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        for(int i =0;i<image_width;i++){
+            auto pixel_color = color(double(i)/(image_width-1),double(j)/(image_height-1),0);
+            write_color(std::cout,pixel_color);
         }
     }
 
-    std::clog<< "\rDone         \n";
+    std::clog << "\rDone.               \n";
 }
