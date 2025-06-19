66587d74a78d547434ca3167adefe3685faaa58a
Sahan <rsahanediriweera@gmail.com>
2025-06-19 23:02:28 +0530

    feat:render-black-image

--- ppm-image-format/src/main.cpp
+++ ppm-image-format/src/main.cpp
@@ -1,23 +1,58 @@
 #include "lib/color.h"
+#include "lib/ray.h"
 #include "lib/vec3.h"
 
 #include <iostream>
 
+color ray_color(const ray& r){
+    return color(0,0,0);
+}
+
 int main() {
 
-    int image_width = 256;
-    int image_height = 256;
+    //Image
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
 
-    std::cout << "P3\n" << image_width  << ' ' << image_height << "\n255\n";
+    int image_height = int(image_width/aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+
+    auto viewport_width = viewport_height*(double(image_width)/image_height);
+
+    auto camera_center = point3(0,0,0);
+
+
+    auto viewport_u = vec3(viewport_width,0,0);
+    auto viewport_v = vec3(0,-viewport_height,0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2 ;
+    auto pixel100_loc = viewport_upper_left + 0.5*(pixel_delta_u+pixel_delta_v);
+
+    //Render
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for(int j =0;j<image_height;j++){
-        std::clog << "\rScanlines remaining: " << (image_height -j) << ' ' << std::flush;
+        std::clog << "\rScanlines remaining: " << image_height -j << " " << std::flush;
 
-        for(int i =0;i<image_width;i++){
-            auto pixel_color = color(double(i)/(image_width-1),double(j)/(image_height-1),0);
+        for(int i = 0; i< image_width; i++){
+            auto pixel_center = pixel100_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
+            auto ray_direction = pixel_center + camera_center;
+
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout,pixel_color);
         }
     }
 
-    std::clog << "\rDone.               \n";
+    std::clog << "\rDone";
+
 }
