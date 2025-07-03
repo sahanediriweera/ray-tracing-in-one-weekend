3a8fda352da5e42fb6887e682104990f184fde7d
Sahan <rsahanediriweera@gmail.com>
2025-07-03 22:40:30 +0530

    feat:add-random-double-generators

--- ppm-image-format/src/lib/rtweekend.h
+++ ppm-image-format/src/lib/rtweekend.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -12,10 +13,22 @@ using std::shared_ptr;
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-inline double degrees_to_radians(double degrees){
+inline double degrees_to_radians(double degrees)
+{
     return degrees * pi / 180.0;
+};
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
 
+inline double random_double(double min, double max)
+{
+    return (min + (max - min) * random_double());
+};
 
 #include "color.h"
 #include "ray.h"
